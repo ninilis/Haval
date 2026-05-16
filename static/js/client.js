75645bbe6b262fddca2e2668edad493bec1d2691
ninilis <latifullina1997@mail.ru>
2026-05-16 10:46:05 +0300

    Верстка по пиксель перфект, добавление отработчиков событий, модального окна

--- static/js/client.js
+++ static/js/client.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Переключение цветов машин
-    document.querySelectorAll('.car-card__colors').forEach(function(colorsBlock) {
+document.addEventListener('DOMContentLoaded', function () {
+// Цвет машин
+    document.querySelectorAll('.car-card__colors').forEach(function (colorsBlock) {
         const card = colorsBlock.closest('.car-card');
         const imageBlock = card.querySelector('.car-card__image-block');
         const images = imageBlock.querySelectorAll('.car-image');
         const dots = colorsBlock.querySelectorAll('.color-dot');
-
-        // Начальное состояние: белая машина активна
+        // Начальное состояние: первый цвет активен
         dots[0].classList.add('active');
         images[0].classList.add('active');
-
-        dots.forEach(function(dot) {
-            dot.addEventListener('click', function() {
+        dots.forEach(function (dot) {
+            dot.addEventListener('click', function () {
                 const color = this.getAttribute('data-color');
 
                 dots.forEach(d => d.classList.remove('active'));
@@ -27,11 +25,42 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Плавный показ таймера после отрисовки
-    setTimeout(function() {
+// Модалки
+    setTimeout(function () {
         const timerWrapper = document.querySelector('.timer-wrapper');
         if (timerWrapper) {
             timerWrapper.classList.add('visible');
         }
     }, 600);
+     const modal = document.getElementById('requestModal');
+    if (!modal) return;   // выход, если окна нет на странице
+    const closeBtn = modal.querySelector('.modal-close');
+    const form = document.getElementById('requestForm');
+    const phoneInput = document.getElementById('phone');
+    document.body.addEventListener('click', function (e) {
+        if (
+            e.target.closest('.promotion__button') ||
+            e.target.closest('.btn-card') ||
+            e.target.closest('.car-card__test-drive')
+        ) {
+            e.preventDefault();
+            modal.classList.add('active');
+        }
+    });
+    closeBtn.addEventListener('click', function () {
+        modal.classList.remove('active');
+    });
+    modal.addEventListener('click', function (e) {
+        if (e.target === modal) {
+            modal.classList.remove('active');
+        }
+    });
+    // Отправка формы
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        alert('Форма отправлена. Телефон: ' + phoneInput.value);
+        modal.classList.remove('active');
+        phoneInput.value = '';
+    });
+
 });
